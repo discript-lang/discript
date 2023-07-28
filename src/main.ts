81bbcc4c74378663b4f6b2ab0bbb35d262453591
R-unic <roleyspersonal@gmail.com>
2023-07-28 14:25:16 -0400

    build: make compilation incremental

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import Lexer from "./lexer";
 
-const fileName = "../test.ds";
+const fileName = "test.ds";
 const testContents = readFileSync(fileName);
 const lexer = new Lexer(testContents.toString(), fileName);
 console.log(lexer.tokenize());
